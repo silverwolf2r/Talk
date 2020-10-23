a29a500c4069cf8954c8e32db0428a076706bb9e
silverwolf2r <46742063+silverwolf2r@users.noreply.github.com>
2020-10-22 23:17:28 -0700

    Update Talk.sh

--- Talk.sh
+++ Talk.sh
@@ -3,20 +3,24 @@
 #!/bin/bash
 
 Talk () {
+cd Talk
  read Talk
  echo -e "$Talk" >> CursedWords.txt
  python3 Cursespeak.py
  play DontCurse.mp3
  rm DontCurse.mp3
- rm CursedWords.txt  	  
+ rm CursedWords.txt
+ cd
 }
 
 talk () {
+cd Talk
  read talk
  echo -e "$talk" >> CursedWords.txt
  python3 Cursespeak.py
  play DontCurse.mp3
  rm DontCurse.mp3
  rm CursedWords.txt
+ cd
 }
 
